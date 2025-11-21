2d665a956fc9d68b1faa22bc3eac9efdd4b2f4e6
Alexandre Bleuler <alexandre.bleuler@yahoo.com>
2025-11-21 18:35:46 +0100

    version 2 : random agent + test with sample and manual

--- tests/test_random_agent.py
+++ tests/test_random_agent.py
@@ -1,14 +1,25 @@
-from ..src import random_agent
 from pettingzoo.classic import connect_four_v3
+
+import src.random_agent as rnda
 
 env = connect_four_v3.env(render_mode="human") # ou render_mode="rdb_array" ou bien None
 
 env.reset(seed=42)
 
-print(env.agents)
+agent0=rnda.RandomAgent(env,env.agents[0])
+agent1=rnda.RandomAgent(env,env.agents[1])
+agent_list=[agent0,agent1]
+
+### Testing RandomAgent with choose_action
 
 for agent in env.agent_iter():
     observation, reward, termination, truncation, info = env.last()
+
+    if agent_list[0].name==agent :
+        current_agent=agent_list[0]
+
+    else :
+        current_agent=agent_list[1]
 
     if termination or truncation:
         action = None
@@ -18,11 +29,36 @@ for agent in env.agent_iter():
             print("It's a draw!")
     else:
         # Take a random valid action
-        mask = observation["action_mask"]
-        action = env.action_space(agent).sample(mask)
+        action = current_agent.choose_action(observation)
+        print(f"{agent} plays column {action}")
+
+    env.step(action)
+
+
+### Testing RandomAgent with choose_action_manual
+
+env.reset(seed=42)
+
+for agent in env.agent_iter():
+    observation, reward, termination, truncation, info = env.last()
+
+    if agent_list[0].name==agent :
+        current_agent=agent_list[0]
+
+    else :
+        current_agent=agent_list[1]
+
+    if termination or truncation:
+        action = None
+        if reward == 1:
+            print(f"{agent} wins!")
+        elif reward == 0:
+            print("It's a draw!")
+    else:
+        # Take a random valid action
+        action = current_agent.choose_action_manual(observation)
         print(f"{agent} plays column {action}")
 
     env.step(action)
 
 env.close()
-
